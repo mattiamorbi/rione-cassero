8b7ea74b0053d701c96d06055ed7e014c7bcc53d
Mattia <mattia.morbidelli@gmail.com>
2024-12-21 11:35:22 +0100

    tolto il while nella funzione che ritorna la data dal server ora la richiesta viene fatta una volta sola

--- lib/helpers/server_date.dart
+++ lib/helpers/server_date.dart
@@ -1,27 +1,20 @@
 import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 Future<DateTime?> fetchCurrentDateTime() async {
   DateTime? date;
+  final response = await http.get(Uri.parse(
+      'https://api.timezonedb.com/v2.1/get-time-zone?key=4PWINFC67FAE&format=json&by=zone&zone=Europe/Rome'));
 
-  while (date == null) {
-    final response = await http.get(Uri.parse(
-        'https://api.timezonedb.com/v2.1/get-time-zone?key=4PWINFC67FAE&format=json&by=zone&zone=Europe/Rome'));
-
-
-    if (response.statusCode == 200) {
-      if (kDebugMode) {
-        print(response.body);
-      }
-      Map<String, dynamic> data = jsonDecode(response.body);
-      String dateTimeString = data['formatted']; // Ottieni la stringa della data
-      date = DateTime.parse(dateTimeString);
-      return date; // Convdaerte in DateTime
-    } else {
-      await Future.delayed(Duration(seconds: 1));
-      throw Exception('Failed to load date time');
+  if (response.statusCode == 200) {
+    if (kDebugMode) {
+      print(response.body);
     }
-
+    Map<String, dynamic> data = jsonDecode(response.body);
+    String dateTimeString = data['formatted']; // Ottieni la stringa della data
+    date = DateTime.parse(dateTimeString);
+    return date; // Convdaerte in DateTime
   }
 }
